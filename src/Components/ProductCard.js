a5c90db0a9bba819a0d2939c2695db86b1c5d674
Yousuf Chohan <buoycho1999@gmail.com>
2024-01-05 20:39:36 +0500

    Added new Job Cards

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -6,7 +6,7 @@ const ProductCard = () => (
   <div className="card-wrapper">
     <div className="card">
       <div className="card-content">
-        <p className="card-title">Awesome Product</p>
+        <p className="card-title">Awesome Product | $45 - $48</p>
         <p className="card-body">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio,
           culpa.
